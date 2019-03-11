5d18d4ef76113df05bacbb040ecf2657a5abeea5
soramimi <fi7s-fct@asahi-net.or.jp>
2019-03-11 12:07:18 +0900

    .

--- files/initdata.sh
+++ files/initdata.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /var/www/redmine && RAILS_ENV=production bundle exec rake db:migrate
 cd /var/www/redmine && RAILS_ENV=production REDMINE_LANG=ja bundle exec rake redmine:load_default_data
+chown www-data.www-data /var/www/redmine -R
